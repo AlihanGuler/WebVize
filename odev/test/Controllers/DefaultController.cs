2dc1231c95cb876984f465b0ca9cda5113ce6f4c
AlihanGuler <60668272+AlihanGuler@users.noreply.github.com>
2020-12-07 21:55:46 +0300

    odev

---
+++ odev/test/Controllers/DefaultController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Takım()
+        {
+            return View(Models.Class1.NBAtakımları);
+        }
+        public ActionResult Edit(int Id)
+        {
+            var nba = Models.Class1.NBAtakımları.Where(b => b.Id == Id).FirstOrDefault();
+            return View(nba);
+        }
+        [HttpPost]
+
+        public ActionResult Edit(Models.Class1 nbaa)
+        {
+            var nba = Models.Class1.NBAtakımları.Where(b => b.Id == nbaa.Id).FirstOrDefault();
+            nba.Id = nbaa.Id;
+            nba.TakımAdı = nbaa.TakımAdı;
+            nba.Fiyat = nbaa.Fiyat;
+            return RedirectToAction("Takım");
+        }
+        public ActionResult Delete(int Id)
+
+        {
+
+            var takımtakım = Models.Class1.NBAtakımları.Where(b => b.Id == Id).FirstOrDefault();
+
+            return View(takımtakım);
+
+        }
+        [HttpPost]
+        public ActionResult Delete(Models.Class1 tkm)
+
+        {
+
+            var szz = Models.Class1.NBAtakımları.Where(b => b.Id == tkm.Id).FirstOrDefault();
+            Models.Class1.NBAtakımları.Remove(szz);
+            return RedirectToAction("Takım");
+
+        }
+
+
+
+    }
+}
---
+++ odev/test/Models/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test.Models
+{
+    public class Class1
+    {
+        
+        public int Id { get; set; }
+        public string TakımAdı { get; set; }
+        public string Fiyat { get; set; }
+
+
+        public static List<Class1> NBAtakımları = new List<Class1>
+        {
+                 new Class1
+             {
+                 Id=1,
+                 TakımAdı="LA Lakers Forma",
+                 Fiyat="100$",
+             },
+                  new Class1
+             {
+                 Id=2,
+                 TakımAdı="Golden State Warriors",
+                 Fiyat="65$",
+             },
+                   new Class1
+             {
+                 Id=3,
+                 TakımAdı="Brooklyn Nets",
+                 Fiyat="139$",
+             },
+                   new Class1
+                   {
+                 Id=4,
+                 TakımAdı="Cleveland Cavaliers",
+                 Fiyat="99$",
+             }
+        };
+    }
+}
